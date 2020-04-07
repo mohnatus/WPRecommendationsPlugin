1eebbb5135ef5d258b5d0a1c7f31bd23f4e0229e
FurryCat <furrycat.web@gmail.com>
2020-04-07 22:01:24 +0300

    add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+
+if( ! defined('WP_UNINSTALL_PLUGIN') ) exit;
+
+delete_option('recommendations_custom_css_option');
+delete_option('recommendations_options');
+delete_option('recommendations_disable');
